077038d2bdceb630cee6ab698196afeba903f58f
Alex Stewart <alex.stewart14361@gmail.com>
2020-12-28 16:51:21 +1300

    Enlarged the planets and changed the lighting style from hemisphere to both hemisphere and point light.

--- src/index.js
+++ src/index.js
@@ -9,31 +9,36 @@ const startApp = () => {
         scene.clearColor = new BABYLON.Color3.Black();
 
         // create the camera
-        var camera = new BABYLON.ArcRotateCamera("Camera", 0, Math.PI / 2, 12, BABYLON.Vector3.Zero(), scene);
+        const camera = new BABYLON.ArcRotateCamera("Camera", 0, Math.PI / 2, 12, BABYLON.Vector3.Zero(), scene);
         camera.attachControl(canvas, true);
 
-        // create the lighting
-        const light = new BABYLON.HemisphericLight("light1", new BABYLON.Vector3(100, 100, 100), scene);
+        // sun light
+        const light0 = new BABYLON.PointLight("Omni0", new BABYLON.Vector3(0, 0, 0), scene);
+        light0.diffuse = new BABYLON.Color3(1, 1, 1);
+
+        // top light to illuminate the grid
+        const light1 = new BABYLON.HemisphericLight("HemiLight", new BABYLON.Vector3(0, 100, 100), scene);
 
         // create a mesh ground
     	const ref = BABYLON.Mesh.CreateGround("gnd", 100, 100, 100, scene);
 	    ref.material = new BABYLON.StandardMaterial("gmat", scene);
         ref.material.wireframe = true;
-        ref.material.alpha = 0.1;
+        ref.material.alpha = 0.05;
 
         // create the planets
-        const sun = BABYLON.Mesh.CreateSphere("sphere", 32, 2, scene);
-        const earth = BABYLON.Mesh.CreateSphere("sphere", 32, 0.5, scene);
-        const earthMoon = BABYLON.Mesh.CreateSphere("sphere", 32, 0.1, scene);
-        const mercury = BABYLON.Mesh.CreateSphere("sphere", 32, 0.3, scene);
-        const venus = BABYLON.Mesh.CreateSphere("sphere", 32, 0.4, scene);
-        const mars = BABYLON.Mesh.CreateSphere("sphere", 32, 0.4, scene);
-        const jupiter = BABYLON.Mesh.CreateSphere("sphere", 32, 1.2, scene);
-        const saturn = BABYLON.Mesh.CreateSphere("sphere", 32, 0.9, scene);
+        const sun = BABYLON.Mesh.CreateSphere("sphere", 32, 5, scene);
+        const earth = BABYLON.Mesh.CreateSphere("sphere", 32, 1, scene);
+        const earthMoon = BABYLON.Mesh.CreateSphere("sphere", 32, 0.5, scene);
+        const mercury = BABYLON.Mesh.CreateSphere("sphere", 32, 1, scene);
+        const venus = BABYLON.Mesh.CreateSphere("sphere", 32, 1, scene);
+        const mars = BABYLON.Mesh.CreateSphere("sphere", 32, 1, scene);
+        const jupiter = BABYLON.Mesh.CreateSphere("sphere", 32, 2.5, scene);
+        const saturn = BABYLON.Mesh.CreateSphere("sphere", 32, 2, scene);
 
         // add color to the planets
         sun.material = new BABYLON.StandardMaterial("sunmaterial", scene);
         sun.material.diffuseTexture = new BABYLON.Texture("style/textures/sun.jpg", scene);
+        sun.material.emissiveColor = new BABYLON.Color3(1, 1, 0);
 
         mercury.material = new BABYLON.StandardMaterial("sunmaterial", scene);
         mercury.material.diffuseTexture = new BABYLON.Texture("style/textures/mercury.jpg", scene);
@@ -56,8 +61,10 @@ const startApp = () => {
         saturn.material = new BABYLON.StandardMaterial("sunmaterial", scene);
         saturn.material.diffuseTexture = new BABYLON.Texture("style/textures/saturn.jpg", scene);
 
-        const saturnRings = BABYLON.Mesh.CreateTorus("sphere", 2,0.5,40, scene);
+        const saturnRings = BABYLON.Mesh.CreateTorus("sphere", 3.5, 0.8, 40, scene);
         saturnRings.scaling.y = 0.01;
+        saturnRings.material = new BABYLON.StandardMaterial("sunmaterial", scene);
+        saturnRings.material.diffuseTexture = new BABYLON.Texture("style/textures/saturn.jpg", scene);
         
         let earthAlpha = Math.PI;
         let mercuryAlpha = Math.PI;
@@ -65,6 +72,7 @@ const startApp = () => {
         let marsAlpha = Math.PI;
         let JupiterAlpha = Math.PI;
         let saturnAlpha = Math.PI;
+        let earthMoonAlpha = Math.PI;
 
         scene.beforeRender = () => {
             // calculate the orbits of the planets
@@ -75,11 +83,16 @@ const startApp = () => {
             jupiter.position = new BABYLON.Vector3(30 * Math.sin(JupiterAlpha), sun.position.y, 30 * Math.cos(JupiterAlpha));
             saturn.position = new BABYLON.Vector3(40 * Math.sin(saturnAlpha), sun.position.y, 40 * Math.cos(saturnAlpha));
             saturnRings.position = new BABYLON.Vector3(40 * Math.sin(saturnAlpha), sun.position.y, 40 * Math.cos(saturnAlpha));
-            
+            //saturnRings.rotate(0.025, 0, BABYLON.Space.WORLD);
+            earthMoon.position = new BABYLON.Vector3(10 * Math.sin(earthMoonAlpha), earth.position.y, 10 * Math.cos(earthMoonAlpha));
 
+        
             //rotate the planets
             earth.rotation.y += 0.01;
             earthMoon.rotation.y += 5;
+            saturnRings.rotation.y += 3;
+            earthMoon.rotation.y -= 0.01
+            sun.rotation.y += 0.003;
 
             // increment the alpha
             mercuryAlpha += 0.005;
@@ -88,6 +101,7 @@ const startApp = () => {
             marsAlpha += 0.002;
             JupiterAlpha += 0.0009;
             saturnAlpha += 0.0005;
+            earthMoonAlpha += 0.01;
 
         }
 
