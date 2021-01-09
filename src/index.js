d3c4954a1336f704545c86bdd0b753d1de64bfd7
Alex Stewart <alex.stewart14361@gmail.com>
2021-01-09 16:36:03 +1300

    Made functionality that zooms into planets better

--- src/index.js
+++ src/index.js
@@ -79,6 +79,16 @@ const startApp = () => {
 
         planets = createPlanets(scene, planetData);
         let count = 0;
+
+        const reduceAlpha = (planet) => {
+            if(planet.alpha > Math.PI * 2){
+                planet.alpha -= Math.PI * 2;
+                reduceAlpha(planet);
+            }else{
+                return;
+            }
+        }
+
         scene.beforeRender = () => {
 
             renderPlanets(planets);
@@ -93,12 +103,62 @@ const startApp = () => {
                 blockPlanetClick = true;
 
                 if(zoomingIn){
-
+                    // get the alpha, radius and beta positions of the planet
+                    if(count == 0){
+                        
+                        const p = planets[focusCameraOnPlanetId];
+                        console.log(p.alpha);
+                        console.log('spinning');
+                        if(p.alpha > Math.PI * 2){
+                            console.log(p.alpha);
+                            console.log('alpha bigger than one circle');
+                            reduceAlpha(p);
+                            console.log(p.alpha);
+                        }
+                        
+                        setTimeout(()=>camera.spinTo("beta", Math.PI / 2, 100), 10);
+                        setTimeout(()=>camera.spinTo("radius", p.orbit + p.radius + 5, 100), 20);
+                        setTimeout(()=>camera.spinTo("alpha", p.alpha, 100), 30);
+                        console.log('spun');
+                    }else if(count == 100){
+                        zoomingIn = false;
+                        count = -1;
+                        console.log(planets[focusCameraOnPlanetId].alpha);
+                    }
+                    console.log(planets[focusCameraOnPlanetId].alpha);
+                    count++;
+                    
+                    /*
+                    const id = focusCameraOnPlanetId;
                     const p = planets[focusCameraOnPlanetId];
-                    const planetToPos = new BABYLON.Vector3((p.position.x), 0, (p.position.z));
-                    camera.position = BABYLON.Vector3.Lerp(camera.position, planetToPos,0.02);
+
+                    let alphaChange = 0.1;
+                    let distanceChange = 0;
+
+                    if(id == 0 || id == 5){    
+                        distanceChange = 3;
+                    }else if( id == 1 || id == 2 || id == 3 || id == 4 ){
+                        distanceChange = 1;
+                    }else if(id == 6){
+                        distanceChange = 4;       
+                    }else if(id == 7 || id == 8){
+                        distanceChange = 2;       
+                    }
+
+                    const planetToPos = new BABYLON.Vector3((p.orbit + p.radius + distanceChange) * Math.sin(p.alpha + alphaChange), 0, (p.orbit + p.radius +distanceChange) * Math.cos(p.alpha + alphaChange));
+
+                    //const planetToPos = new BABYLON.Vector3((p.position.x) * 1.2, 0, (p.position.z) * 1.2);
+                    camera.position = BABYLON.Vector3.Lerp(camera.position, planetToPos, 0.02);
+                    if(camera.position == planetData.position){
+                        console.log('camera at planet location');
+                    }
+
+
+
+
+                    console.log(camera.position, planetToPos);
                     console.log(count);
-                    if(count == 100){
+                    if(count == 200){
                         zoomingIn = false;
                         console.log('movement done');
                         console.log(p.position);
@@ -106,6 +166,7 @@ const startApp = () => {
                         count = 0;
                     }
                     count++;
+                    */
                 }else{
                     //console.log(camera.position);
                     renderCamera(planets, focusCameraOnPlanetId, camera);
