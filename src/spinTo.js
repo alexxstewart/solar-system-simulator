7a99afedc6644cdf2187e10b1f388295774db297
aste425 <alex.stewart14361@gmail.com>
2021-01-21 20:00:31 +1300

    Added comments to the spinTo.js file

--- src/spinTo.js
+++ src/spinTo.js
@@ -1,3 +1,9 @@
+
+/*
+This prototype function is added to the arcRotateCamera class.
+When this function is called it allows the camera to rotate to a certain position over a speed and dimension.
+Movement can be added to the alpha and beta dimensions
+*/
 export const initiateSpinToFunction = () => {
     BABYLON.ArcRotateCamera.prototype.spinTo = function (whichprop, targetval, speed, fps) {
         var ease = new BABYLON.CubicEase();
